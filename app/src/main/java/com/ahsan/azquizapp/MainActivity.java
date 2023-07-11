b26edbba66c1e7d5b2131ea98955e0e43a37b646
junaid786 <junaidchaudahry@gmail.com>
2023-07-11 23:36:40 +0500

    MainActivity toolbar and set icons

---
+++ app/src/main/java/com/ahsan/azquizapp/MainActivity.java
@@ -0,0 +1,65 @@
+package com.ahsan.azquizapp;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    NavigationView nav;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        nav = findViewById(R.id.navmenu);
+        drawer = findViewById(R.id.drawer);
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new home()).commit();
+        nav.setCheckedItem(R.id.home);
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            Fragment temp;
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.home:
+                        temp = new home();
+                        break;
+
+                    case R.id.quiz:
+                        temp = new TakeQuiz();
+                        break;
+
+                    case R.id.result:
+                        temp = new ShowResult();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,temp).commit();
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+}
